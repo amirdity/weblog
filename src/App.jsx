493627746ccebd3888a29d3933577b591a68addc
haj amir <amirfattahi22@gmail.com>
2024-05-26 10:40:51 +0330

    ok

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+import PostsList from "./components/PostsList";
+
+function App() {
+  return (
+    <main>
+      <PostsList/>
+    </main>
+  );
+}
+
+export default App;
